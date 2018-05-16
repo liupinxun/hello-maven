96bea75a4b3a1a6730fe5eaed7adc36202c97d4c
liupinxun@126.com <liupinxun@126.com>
2018-05-16 14:34:51 +0800

    添加controller

---
+++ src/main/java/com/lpx/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.lpx.controller;
+
+
+/**
+ * Created by liupinxun on 2018/5/16.
+ *  控制层
+ */
+public class Controller {
+    public static void main(String[] args) {
+        System.out.println("hello-maven");
+    }
+}
